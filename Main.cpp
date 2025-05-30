b747aad9bc8da99b09dc548cf5195b2317d3fc9c
KevinUdz <kevinuruel22@gmail.com>
2025-05-30 08:41:13 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,19 +6,22 @@
 #include "Serie.h"
 
 int main() {
-    // Se crea una película con sus datos
-    Pelicula peli1(1, "Inception", 148, "Accion", 4.8);
+    // Creare los punteros a la clase base apuntando a los objetos derivados
+    Video * videos[2];
 
-    // Se crea una serie con sus datos
-    Serie serie1(2, "Stranger Things", 50, "Misterio", 4.7);
+    videos[0] = new Pelicula(1, "Inception", 148, "Accion", 4.8); // Guardo las direcciones de estos objetos para que puedan 'videos' pueda apuntar a una pelicula y el otro a una serie
+    videos[1] = new Serie(2, "Stranger Things", 50, "Misterio", 4.7);
 
-    // Se muestra la información de la película
-    cout << " Informacion de la Pelicula " << endl;
-    peli1.mostrarPelicula();
+    for (int i = 0; i < 2; ++i) {  // implemento un ciclo for para llamarlos
+        cout << "\nCargando video " << i + 1<< ":\n";
+        videos[i]->mostrarDetalles();  // Aqui estaria llamando a 'mostrarDetalles' haciendo uso del puntero '->' que tambien se vio en clase
 
-    // Se muestra la información de la serie
-    cout << "\n Informacion de la Serie " << endl;
-    serie1.mostrarSerie();
+    }
+
+    // Para evitar errores se libera la memoria
+    for (int i = 0; i < 2; i++) {
+        delete videos[i];
+    }
 
     return 0;
 }
